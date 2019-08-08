22ce4445f323ceeab8b44ebb501c5333cafa6ecd
rodrigoronin <liramailbox@gmail.com>
2019-08-08 14:13:00 -0300

    Exportando static site

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const externalCSS = require('@zeit/next-css')
 module.exports = externalCSS()
+
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+module.exports = {
+    exportPathMap: () => ({
+        '/': { page: '/' },
+        '/contact': { page: '/contact' }
+    }),
+    assetPrefix: isProd ? '/luz-do-saber-home-page' : '',
+}
