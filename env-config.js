170610c4070b772c90dfe4c25979b0dfb972aec0
rodrigoronin <liramailbox@gmail.com>
2020-01-15 16:18:33 -0300

    Mudando arquivos para o repositório correto

--- env-config.js
+++ env-config.js
@@ -1,4 +1,4 @@
 const prod = process.env.NODE_ENV === 'production'
 
 // module.exports = { assetPrefix: prod ? 'https://lmedesenvolvimento.github.io/luz-do-saber-home-page/' : '' }
-module.exports = { assetPrefix: prod ? 'https://lmedesenvolvimento.github.io/luz-do-saber/' : '' }
+module.exports = { assetPrefix: prod ? 'https://lmedesenvolvimento.github.io/eja-luz-do-saber/' : '' }
